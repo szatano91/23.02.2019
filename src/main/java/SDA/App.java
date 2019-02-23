66264138247e987fc8b4bbd8889f5a495cef43d1
Szatano <szatano.jakub@gmail.com>
2019-02-23 10:40:07 +0100

    v.1

--- src/main/java/SDA/App.java
+++ src/main/java/SDA/App.java
@@ -5,6 +5,8 @@ public class App
 {
     public static void main( String[] args )
     {
+CountingSort sortowanie = new CountingSort();
 
+sortowanie.countingsort(new int[]{4,2,12,34,1});
     }
 }
--- src/main/java/SDA/CountingSort.java
+++ src/main/java/SDA/CountingSort.java
@@ -1,4 +1,54 @@
 package SDA;
 
 public class CountingSort {
+
+    public void countingsort(int[] tablica) {
+
+
+//        Szukanie najewiekszego elementu
+        int temp = tablica[0];
+        for (int key : tablica) {
+
+            if (temp < key) {
+                temp = key;
+            }
+
+        }
+
+// Nowa kolekcja na podstawie najwiekszego elementu plus zapełnienie ją zerami
+
+        int[] array = new int[temp + 1];
+
+        for (int i = 0; i >= temp; i++) {
+            array[i] = 0;
+
+        }
+//      Dla każdego elementu "tablica" wykonaj array[tablica]++
+
+        for (int key : tablica) {
+            array[key]++;
+        }
+
+        // Sumowanie array
+
+        for (int i = 0; i < temp; i++) {
+            array[i + 1] = array[i] + array[i + 1];
+        }
+//        Tworzenie koncowej + sortowanie
+        int[] finalarray = new int[tablica.length];
+
+
+        for (int temp2 = 0; temp2 < tablica.length;temp2++){
+
+            array[tablica[temp2]]--;
+            finalarray[array[tablica[temp2]]]=tablica[temp2];
+        }
+
+        for (int key : finalarray) {
+            System.out.print(key+" ");
+        }
+
+
+    }
 }
+
